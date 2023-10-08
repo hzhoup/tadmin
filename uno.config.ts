2eed83d952586082af60ef8fab31d4ae86a88f25
hzp <hzhouping@outlook.com>
2023-10-08 15:28:53 +0800

    feat: 统一unocss,element-plus色彩

--- uno.config.ts
+++ uno.config.ts
@@ -11,7 +11,12 @@ export default defineConfig({
   transformers: [transformerDirectives()],
   theme: {
     colors: {
-      primary: 'var(--el-color-primary)'
+      primary: 'var(--el-color-primary)',
+      success: 'var(--el-color-success)',
+      warning: 'var(--el-color-warning)',
+      danger: 'var(--el-color-danger)',
+      error: 'var(--el-color-error)',
+      info: 'var(--el-color-info)'
     }
   }
 })
